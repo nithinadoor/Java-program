0c938337d68a3b8c448a72d70e231639c5539c00
Nithin V Mathew <117814670+nithinadoor@users.noreply.github.com>
2023-06-09 15:37:17 +0530

    Add files via upload

---
+++ Sort.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.Arrays;
+
+class Sort
+{
+	String str="hellgkfromios"
+	char arr[]=str.toCharArray();
+	
+	char temp;
+	int i=0;
+	while(i<arr.length)
+	{
+		int j=i+1;
+			
+	}
+}
+
